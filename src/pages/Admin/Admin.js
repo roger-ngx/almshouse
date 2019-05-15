bb37a9d403406a05c126658dcc5c328e4e259ff8
thanh90 <30317015+thanh90@users.noreply.github.com>
2019-05-15 23:41:08 +0900

    add an admin page for uploading images to firestore

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+import { map } from 'lodash';
+
+import DevTools from 'mobx-react-devtools';
 
 const Admin = ({AdminStore}) => {
-    const handleFilesChanged = e => AdminStore.uploadImagesToStorage(e.target.files[0]);
+    const handleFilesChanged = e => AdminStore.uploadImagesToStorage(e.target.files);
+
+    console.log(AdminStore.uploadedImageUrls);
 
     return (
         <div>
             <input type='file' onChange={handleFilesChanged} multiple />
+            <div>
+                {
+                   map(AdminStore.uploadedImageUrls, imageUrl => <img alt='' src={imageUrl} style={{width: '100px', height: '100px'}}/>)
+                }
+            </div>
+            <DevTools />
         </div>
     )
 }
--- src/stores/AdminStore.js
+++ src/stores/AdminStore.js
@@ -1,14 +1,35 @@
 import firebase from '../config/Firebase';
+import { decorate, observable, action, runInAction } from 'mobx';
+import { forEach } from 'lodash';
 
 class AdminStore {
-    uploadImagesToStorage = (image) => {
-        const storageRef = firebase.storage().ref();
-        const imageRef = storageRef.child(`images/${image.name}`);
+    uploadedImageUrls = [];
 
-        imageRef.put(image)
-        .then(snapshot => console.log(snapshot))
-        .catch(err => console.log(err));
+    uploadImagesToStorage = (images) => {
+        const storageRef = firebase.storage().ref();
+
+        forEach(images, async (image) => {
+            const imageRef = storageRef.child(`images/${image.name}`);
+            try{
+                const snapshot = await imageRef.put(image)
+            
+                const downloadUrl = await snapshot.ref.getDownloadURL();
+
+                runInAction(()=>{
+                    this.uploadedImageUrls = [downloadUrl, ...this.uploadedImageUrls];
+                    console.log(this.uploadedImageUrls);
+                });
+
+            }catch(err){
+                console.log(err);
+            }
+        });
     }
 }
+
+decorate(AdminStore, {
+    uploadedImageUrls: observable,
+    uploadImagesToStorage: action
+})
 
 export default new AdminStore();
